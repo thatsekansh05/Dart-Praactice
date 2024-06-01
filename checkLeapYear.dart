2d8cbd811ee10f822a8cc89bfe97f5f245359a39
Ekansh Kunchal <168631833+thatsekansh05@users.noreply.github.com>
2024-06-01 15:00:56 +0530

    Add files via upload
     Write a program that checks if a year is a leap year. If it is a leap year, print "Leap year." If it is not, print "Not a leap year."

---
+++ checkLeapYear.dart
@@ -0,0 +1,9 @@
+void main() {
+  int a = 2022;
+  print("The Year You entered is $a.");
+  if (a % 4 == 0) {
+    print("$a is a Leap Year.");
+  } else {
+    print("$a is not a Leap Year.");
+  }
+}
