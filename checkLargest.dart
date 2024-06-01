a7178977c18b58050c851e09f55deae2703ba8c0
Ekansh Kunchal <168631833+thatsekansh05@users.noreply.github.com>
2024-06-01 14:02:58 +0530

    Add files via upload
    Write a program that compares two numbers and prints which number is larger.

---
+++ checkLargest.dart
@@ -0,0 +1,11 @@
+void main() {
+  int a = 23;
+  int b = 24;
+  if (a > b) {
+    print("$a is greater than $b.");
+  } else if (b > a) {
+    print("$b is Greater than $a.");
+  } else {
+    print("$a and $b are Equal.");
+  }
+}
