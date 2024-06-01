affd21aaf61cbb018c9fac48531b70343dedd99d
Ekansh Kunchal <168631833+thatsekansh05@users.noreply.github.com>
2024-06-01 14:53:37 +0530

    Update checkGrade.dart

--- checkGrade.dart
+++ checkGrade.dart
@@ -4,18 +4,28 @@ void main() {
   print("Enter the marks Of the Student.");
   int? a = int.parse(stdin.readLineSync()!);
   print("The Marks Of the student is $a");
-  int r = a ~/ 10;
-  if (a >= 90 && a <= 100) {
+  if (a >= 90 && a <= 100) 
+  {
     print("The Student got A Grade.");
-  } else if (a >= 80 && a < 90) {
+  } 
+  else if (a >= 80 && a < 90) 
+  {
     print("The Student got B Grade.");
-  } else if (a >= 70 && a < 80) {
+  } 
+  else if (a >= 70 && a < 80) 
+  {
     print("The Student got C Grade.");
-  } else if (a >= 60 && a < 70) {
+  }
+  else if (a >= 60 && a < 70) 
+  {
     print("The Student got D Grade.");
-  } else if (a >= 0 && a < 60) {
+  }
+  else if (a >= 0 && a < 60)
+  {
     print("The Student Failed The Class.");
-  } else {
+  }
+  else 
+  {
     print("The Marks Entered are Wrong.");
   }
 }
